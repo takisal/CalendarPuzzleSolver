09a82ab480f6e3befbd3a9e76e5761ee1ab1b3e0
takisal <33737751+takisal@users.noreply.github.com>
2024-01-08 16:39:53 -0500

    Update requestDate.js

--- requestDate.js
+++ requestDate.js
@@ -1,6 +1,7 @@
 const dayNumbers = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
 ];
+
 const weekdays = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 const weekdaysSet = new Set(weekdays);
